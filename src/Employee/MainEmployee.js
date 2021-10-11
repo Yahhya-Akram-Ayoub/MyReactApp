69519b90fc3561c5818b401887dffa961df71bc7
YahhyaAK <akramyahhya@gmail.com>
2021-10-11 22:07:08 +0300

    first commit

---
+++ src/Employee/MainEmployee.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+export default class MainEmployee extends React.Component{
+
+    constructor() {
+        super();
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return(<>
+
+        </>);
+    }
+
+}
